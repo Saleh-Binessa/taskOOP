5e8d3a1db7a11a0aa6f403dc6fb67d2493c5a754
Saleh-Binessa <saleh.binessa@gmail.com>
2024-04-15 11:04:43 +0300

    solve conflicts

--- taskOOP/Program.cs
+++ taskOOP/Program.cs
@@ -1,10 +1,7 @@
-﻿
-// See https://aka.ms/new-console-template for more information
-
+﻿// See https://aka.ms/new-console-template for more information
 //C# OOP
 
 //Task 1:
-
 public class Student
 {
     // Properties
